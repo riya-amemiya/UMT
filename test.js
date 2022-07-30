07a52fc363cf03083776dbe214d698d669927132
Riya <hiyoriorria@gmail.com>
2022-07-30 22:03:06 +0900

    電卓に機能追加^

---
+++ test.js
@@ -0,0 +1,4 @@
+const calculator = require('./module/Math').calculator
+console.log( '====================================' )
+console.log(calculator('2x + 3 = 5'));
+console.log('====================================');
