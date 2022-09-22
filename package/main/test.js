0608facfb4a5c12aa2bdeb18934a035bc4fa4c3c
Riya <hiyoriorria@gmail.com>
2022-09-22 14:59:33 +0900

    testcode

--- package/main/test.js
+++ package/main/test.js
@@ -1,18 +1,21 @@
 const compilerCore = require('./module/Compiler/core').default;
 const tokenList = [
-    ['PRINT', /print/g, 5],
-    ['STRING', /"[^"]*"/g, 4],
-    ['OPEN_PARENTHESIS', /\(/g, 0],
-    ['CLOSE_PARENTHESIS', /\)/g, 0],
-    ['INT', /\d+/g, 3],
-    ['PLUS', /\+/g, 2],
-    ['MINUS', /-/g, 2],
-    ['MULTIPLY', /\*/g, 1],
-    ['DIVIDE', /\//g, 1],
+    ['PRINT', /print/, 6],
+    ['STRING', /"[^"]*"/, 1],
+    ['OPEN_PARENTHESIS', /\(/, 1],
+    ['CLOSE_PARENTHESIS', /\)/, 1],
+    ['INT', /\d+/, 4],
+    ['PLUS', /\+/, 3],
+    ['MINUS', /-/, 3],
+    ['MULTIPLY', /\*/, 2],
+    ['DIVIDE', /\//, 2],
+    ['COMMENT', /#.*\n/, null],
+    ['EQUAL', /=/, 3],
+    ["NAME",/^(?!.*").+$/,0]
 ];
 const process = (_code, _tokenList, x) => {
     if (x.name === 'PRINT') {
-        return `console.log`;
+        return `puts`;
     } else {
         return x.value;
     }
@@ -20,7 +23,7 @@ const process = (_code, _tokenList, x) => {
 console.log('====================================');
 console.log(
     compilerCore(
-        `print("Hello")\nprint(10 + 20 * 30)`,
+        `print("Hello")\nprint(10 + 20 * 30)\nage = 10`,
         tokenList,
         process,
     ),
