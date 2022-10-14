a299d7dd87554bff4d287cab5c69bf8f2c74c7b2
Riya <hiyoriorria@gmail.com>
2022-10-15 01:56:26 +0900

    新機能の追加

--- package/main/src/Tool/index.ts
+++ package/main/src/Tool/index.ts
@@ -1,5 +1,8 @@
 import { birthday } from './birthday';
 import { dayOfWeek } from './dayOfWeek';
+import { isBrowser } from './isBrowser';
+import { isNode } from './isNode';
+import { isNodeWebkit } from './isNodeWebkit';
 import { pipeFunction } from './pipeFunction';
 
 export { birthday, dayOfWeek, pipeFunction };
@@ -8,10 +11,16 @@ export class UMTToolClass {
     birthday: typeof birthday;
     dayOfWeek: typeof dayOfWeek;
     pipeFunction: typeof pipeFunction;
+    isBrowser: boolean;
+    isNode: boolean;
+    isNodeWebkit: boolean;
     constructor() {
         this.birthday = birthday;
         this.dayOfWeek = dayOfWeek;
         this.pipeFunction = pipeFunction;
+        this.isBrowser = isBrowser;
+        this.isNode = isNode;
+        this.isNodeWebkit = isNodeWebkit;
     }
 }
 
