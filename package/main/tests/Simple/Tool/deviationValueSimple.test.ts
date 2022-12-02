8d395b6de5e964be0ab0b2c0d6580f92fdd847c1
Riya <hiyoriorria@gmail.com>
2022-12-03 00:40:00 +0900

    バグ修正

--- package/main/tests/Simple/Tool/deviationValueSimple.test.ts
+++ package/main/tests/Simple/Tool/deviationValueSimple.test.ts
@@ -1,4 +1,4 @@
-import { deviationValueSimple } from '../../../module/Simple/Math/deviationValueSimple';
+import { deviationValueSimple } from '../../../module/Simple/Tool/deviationValueSimple';
 test('deviationValueSimple', () => {
     expect(deviationValueSimple(100, 50, 10)).toBe(100);
     expect(deviationValueSimple(80, [60, 70, 80, 95, 95])).toBe(50);
