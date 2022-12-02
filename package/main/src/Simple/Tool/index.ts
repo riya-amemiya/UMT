4a63fa82c17feb384a225d305cff23afcdfb0295
Riya <hiyoriorria@gmail.com>
2022-12-03 00:33:36 +0900

    testの追加

--- package/main/src/Simple/Tool/index.ts
+++ package/main/src/Simple/Tool/index.ts
@@ -1,11 +1,23 @@
 import { birthdaySimple } from './birthday';
+import { dayOfWeekSimple } from './dayOfWeekSimple';
+import { deviationValueSimple } from './deviationValueSimple';
 
 export class UMTSimpleToolClass {
     #Local_birthday: typeof birthdaySimple;
+    #Local_dayOfWeek: typeof dayOfWeekSimple;
+    #Local_deviationValue: typeof deviationValueSimple;
     constructor() {
+        this.#Local_dayOfWeek = dayOfWeekSimple;
+        this.#Local_deviationValue = deviationValueSimple;
         this.#Local_birthday = birthdaySimple;
     }
     get birthday() {
         return this.#Local_birthday;
+    }
+    get dayOfWeek() {
+        return this.#Local_dayOfWeek;
+    }
+    get deviationValue() {
+        return this.#Local_deviationValue;
     }
 }
---
+++ package/main/tests/Simple/Tool/dayOfWeekSimple.test.ts
@@ -0,0 +1,5 @@
+import { dayOfWeekSimple } from '../../../module/Simple/Math/dayOfWeekSimple';
+test('dayOfWeekSimple', () => {
+    expect(dayOfWeekSimple('2019-01-01')).toBe(2);
+    expect(dayOfWeekSimple('2020/01/02')).toBe(4);
+});
