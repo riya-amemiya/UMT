47e7f5876b9468c38396d4caa18b6bb082b57d61
Riya Amemiya <riyaamemiya@gmail.com>
2023-04-22 18:56:20 +0900

    コンフリクト解消

--- package/main/src/Date/index.ts
+++ package/main/src/Date/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { now } from './now';
 
 export { now };
@@ -10,6 +11,26 @@ export class UMTDateClass {
     get now() {
         return this.localNow;
     }
+=======
+import { newDate } from "./new";
+import { now } from "./now";
+
+export { now, newDate };
+
+export class UMTDateClass {
+	private localNow: typeof now;
+	private localNewDate: typeof newDate;
+	constructor() {
+		this.localNow = now;
+		this.localNewDate = newDate;
+	}
+	get now() {
+		return this.localNow;
+	}
+	get newDate() {
+		return this.localNewDate;
+	}
+>>>>>>> adf67e3 (機能追加)
 }
 
 export const UMT_Date = new UMTDateClass();
