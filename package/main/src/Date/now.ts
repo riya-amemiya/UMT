d9e3ea26587b4474654dbe6f307b17cfaf39c3f0
Riya Amemiya <riyaamemiya@gmail.com>
2023-04-29 21:40:18 +0900

    説明を追加

--- package/main/src/Date/now.ts
+++ package/main/src/Date/now.ts
@@ -1,5 +1,6 @@
 /**
- * @param {number} [timeDifference=0] 時差(時)
+ * タイムゾーンに関係なく、UTCを基準に指定した時差を加えた現在時刻を取得します。
+ * @param {number} [timeDifference=9] UTCからの時差を指定します。デフォルトは日本時間です。
  * @returns {Date} 現在時刻
  */
 export const now = (timeDifference = 9): Date => {
