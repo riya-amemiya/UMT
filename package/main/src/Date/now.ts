c3d0a33499d7e809b5a75c9cd1bf4d97229dcfc3
Riya Amemiya <riyaamemiya@gmail.com>
2023-04-29 21:34:41 +0900

    最適化

--- package/main/src/Date/now.ts
+++ package/main/src/Date/now.ts
@@ -3,16 +3,5 @@
  * @returns {Date} 現在時刻
  */
 export const now = (timeDifference = 9): Date => {
-	const timezoneOffset = new Date().getTimezoneOffset();
-	const localTimeDifference = -timezoneOffset / 60;
-	const n =
-		localTimeDifference === timeDifference
-			? timeDifference * 2
-			: localTimeDifference + timeDifference;
-	return new Date(
-		Date.now() +
-			(timezoneOffset + (timezoneOffset === 0 ? timeDifference : n) * 60) *
-				60 *
-				1000,
-	);
+	return new Date(Date.now() + timeDifference * 60 * 60 * 1000);
 };
