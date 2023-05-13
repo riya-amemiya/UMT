bff6023ebd40a8f033085a3059800977d329758e
Yuta Nishi <riyaamemiya@gmail.com>
2023-05-13 12:59:48 +0900

    テストファイルを修正

--- package/main/tests/Array/arrayMap.test.ts
+++ package/main/tests/Array/arrayMap.test.ts
@@ -1,9 +1,9 @@
-import { map } from "../../module/Array/map";
+import { arrayMap } from "../../module/Array/arrayMap";
 test("{map}", () => {
-	expect(map([1, 2, 3], (value) => value * 2)).toEqual(
+	expect(arrayMap([1, 2, 3], (value) => value * 2)).toEqual(
 		expect.arrayContaining([2, 4, 6]),
 	);
-	expect(map([1, 2, 3], (value) => value * 2)).toHaveLength(3);
-	expect(map([1, 2, 3], (value) => value * 2)).not.toContain(1);
-	expect(map([1, 2, 3], (value) => value * 2)).not.toContain(3);
+	expect(arrayMap([1, 2, 3], (value) => value * 2)).toHaveLength(3);
+	expect(arrayMap([1, 2, 3], (value) => value * 2)).not.toContain(1);
+	expect(arrayMap([1, 2, 3], (value) => value * 2)).not.toContain(3);
 });
