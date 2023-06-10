0d2f6295d958b882d55b9e8ca4756b2d12d781cc
Riya Amemiya <riyaamemiya@gmail.com>
2023-06-10 21:07:58 +0900

    コンフリクト解消

--- package/main/src/Date/now.ts
+++ package/main/src/Date/now.ts
@@ -1,8 +1,10 @@
+import { hoursTypeInt } from "@/types/clockType";
+
 /**
  * タイムゾーンに関係なく、UTCを基準に指定した時差を加えた現在時刻を取得します。
- * @param {number} [timeDifference=9] UTCからの時差を指定します。デフォルトは日本時間です。
+ * @param {hoursTypeInt} [timeDifference=9] UTCからの時差を指定します。デフォルトは日本時間です。
  * @returns {Date} 現在時刻
  */
-export const now = (timeDifference = 9): Date => {
+export const now = (timeDifference: hoursTypeInt = 9): Date => {
   return new Date(Date.now() + timeDifference * 60 * 60 * 1000);
 };
