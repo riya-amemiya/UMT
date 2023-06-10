bdaf3c706b8ba754dcf71f49fab57f76942900de
Riya Amemiya <riyaamemiya@gmail.com>
2023-06-10 21:10:29 +0900

    コンフリクト解消

--- package/main/test.js
+++ package/main/test.js
@@ -1,45 +1,13 @@
-const { arrayMap } = require("./module/Array/arrayMap");
-const { objectMap } = require("./module/Object/objectMap");
-const inisalArray = Array.from({ length: 1000000 }, (_, i) => i + 1);
-const inisalObject = Object.fromEntries(
-  Array.from({ length: 1000000 }, (_, i) => [i, i + 1]),
-);
-const callbackfn = (item) => {
-  let n = item ** 2;
-  while (n !== 1) {
-    if (n % 2 === 0) {
-      n = n / 2;
-    } else {
-      n = n * 3 + 1;
-    }
-  }
-  return n;
-};
-const answer = inisalArray.map(callbackfn);
+const { DateWrapper } = require("./module/Date/DateWrapper");
+const date1 = new Date("2021-01-01T09:30");
+const date2 = new DateWrapper(date1);
 console.log("====================================");
-console.time("dmap:array");
-const arr1 = [...inisalArray];
-const newArr1 = arr1.map(callbackfn);
-console.log(newArr1.toString() === answer.toString());
-console.timeEnd("dmap:array");
-
-console.time("dmap:object");
-const obj1 = { ...inisalObject };
-const newObj1 = Object.values(obj1).map(callbackfn);
-console.log(newObj1.toString() === answer.toString());
-console.timeEnd("dmap:object");
+console.log("date1", date1);
+console.log("date1.hours", date1.getHours());
+console.log("date1.UTC.hours", date1.getUTCHours());
 console.log("====================================");
 
-console.time("map:array");
-const arr2 = [...inisalArray];
-const newArr2 = arrayMap(arr2, callbackfn);
-console.log(newArr2.toString() === answer.toString());
-console.timeEnd("map:array");
-
-console.time("map:object");
-const obj2 = { ...inisalObject };
-const newObj2 = objectMap(obj2, callbackfn);
-console.log(newObj2.toString() === answer.toString());
-console.timeEnd("map:object");
-
+console.log("====================================");
+console.log("date2", date2);
+console.log("date2.hours", date2.getHour());
 console.log("====================================");
