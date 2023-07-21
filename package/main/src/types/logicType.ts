c945357c2ec86d742ae05a165855a6a72bfdd83f
Yuta Nishi <riyaamemiya@gmail.com>
2023-07-21 13:20:12 +0900

    refactor(types): ロジックタイプの説明文を修正

--- package/main/src/types/logicType.ts
+++ package/main/src/types/logicType.ts
@@ -164,7 +164,7 @@ export type binaryFullAdder<
     >
   : never;
 
-// マイナスを扱えないが任意のバイト数の加算器
+// 任意のバイト数の加算器
 type binaryFullAdderParser<
   X extends string,
   Y extends string,
@@ -180,7 +180,7 @@ type binaryFullAdderParser<
     : never
   : `${A}${C}`;
 
-// マイナスを扱えるが1バイトまでの加算器
+// 1バイトまでの加算器
 type binaryAddParser<
   A extends string,
   B extends string,
