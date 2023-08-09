b1b17349f3f02473dcd179d90416781f9e9400e5
Yuta Nishi <riyaamemiya@gmail.com>
2023-08-09 09:58:38 +0900

    chore(calculator): 電卓モジュールのファイル構造を整理しました。

--- package/main/src/Math/calculator/index.ts
+++ package/main/src/Math/calculator/index.ts
@@ -1,20 +1,13 @@
+import { calculator } from "./calculator";
+import { calculatorInitialization } from "./calculatorInitialization";
 import { calculatorCore } from "./core";
+import { exchange } from "./exchange";
 import { literalExpression } from "./literalExpression";
 
-/**
- * 電卓
- * ()や符号に対応
- * 一文字までの方程式に対応
- * @param  {string} x
- * @param  {object} exchange 為替
- */
-
-export const calculator = <T extends object>(x: string, exchange?: T) => {
-  let copyX = x;
-  copyX = copyX.replace(/\s+/g, ""); // Remove spaces
-  if (copyX.indexOf("=") !== -1) {
-    return literalExpression(copyX); // If the expression contains an equal sign, then it is a literal expression
-  } else {
-    return calculatorCore(copyX, exchange);
-  }
+export {
+  calculator,
+  calculatorInitialization,
+  calculatorCore,
+  exchange,
+  literalExpression,
 };
