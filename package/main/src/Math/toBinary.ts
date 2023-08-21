2f02778c5d72bf48408862985d4aa93dbbade0dc
Riya Amemiya <riyaamemiya@gmail.com>
2023-08-21 23:02:29 +0900

    refactor(Math): 不要なTOBINARYインターフェースを削除し、toBinary関数を単純化

--- package/main/src/Math/toBinary.ts
+++ package/main/src/Math/toBinary.ts
@@ -1,15 +1,11 @@
-export interface TOBINARY {
-  (): (x: number, radix?: number) => string;
-  (x: number, radix?: number): string;
-}
 /**
  * n進数に変換
  * @param {number} x
  * @param  {number} [radix=2] n進数
  */
-export const toBinary = ((x: number, radix = 2) => {
+export const toBinary = (x: number, radix = 2) => {
   if (typeof x === "undefined") {
     return (x: number, radix = 2) => toBinary(x, radix);
   }
   return x.toString(radix);
-}) as TOBINARY;
+};
