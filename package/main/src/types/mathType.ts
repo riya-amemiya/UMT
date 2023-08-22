6e0ddb812cfef3478c50c8f03645a29c53829e6b
Yuta Nishi <riyaamemiya@gmail.com>
2023-08-22 15:28:37 +0900

    fix(types): Moduloの実装を修正

--- package/main/src/types/mathType.ts
+++ package/main/src/types/mathType.ts
@@ -89,9 +89,15 @@ type DivideHelper<
   : DivideHelper<Subtract<X, Y>, Y, Add<A, 1>>;
 
 // 余り
-export type Modulo<A extends number, B extends number> = BGreaterThanA<
-  A,
-  B
-> extends true
+export type Modulo<
+  A extends number,
+  B extends number,
+> = `${A}` extends `-${infer N extends number}`
+  ? `${B}` extends `-${infer M extends number}`
+    ? Modulo<N, M>
+    : Modulo<N, B>
+  : `${B}` extends `-${infer M extends number}`
+  ? Modulo<A, M>
+  : BGreaterThanA<A, B> extends true
   ? A
   : Modulo<Subtract<A, B>, B>;
