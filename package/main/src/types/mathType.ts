740f15880ff30041a043676a83b6365dd7393889
Yuta Nishi <riyaamemiya@gmail.com>
2023-08-22 11:44:44 +0900

    refactor(mathType.ts): たし算のコードをリファクタリングしました

--- package/main/src/types/mathType.ts
+++ package/main/src/types/mathType.ts
@@ -7,9 +7,18 @@ export type NumToArr<
 > = U["length"] extends T ? U : NumToArr<T, [...U, U["length"]]>;
 
 // たし算
-export type Add<X extends number, Y extends number> = Length<
-  [...NumToArr<X>, ...NumToArr<Y>]
->;
+export type Add<
+  X extends number,
+  Y extends number,
+> = `${X}` extends `-${infer N extends number}`
+  ? `${Y}` extends `-${infer M extends number}`
+    ? `-${Add<N, M>}` extends `${infer R extends number}`
+      ? R
+      : never
+    : Subtract<Y, N>
+  : `${Y}` extends `-${infer M extends number}`
+  ? Subtract<X, M>
+  : Length<[...NumToArr<X>, ...NumToArr<Y>]>;
 
 // 引き算
 export type Subtract<A extends number, B extends number> = NumToArr<A> extends [
