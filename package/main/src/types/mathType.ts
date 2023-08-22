463ee24a801a1c5fc911ca73dd3c9ab3c7ab65ce
Yuta Nishi <riyaamemiya@gmail.com>
2023-08-22 14:38:03 +0900

    refactor: 数学関数の型定義を変更しました

--- package/main/src/types/mathType.ts
+++ package/main/src/types/mathType.ts
@@ -21,10 +21,18 @@ export type Add<
   : Length<[...NumToArr<X>, ...NumToArr<Y>]>;
 
 // 引き算
-export type Subtract<A extends number, B extends number> = NumToArr<A> extends [
-  ...NumToArr<B>,
-  ...infer R,
-]
+export type Subtract<
+  A extends number,
+  B extends number,
+> = `${A}` extends `-${infer N extends number}`
+  ? `${B}` extends `-${infer M extends number}`
+    ? Subtract<M, N>
+    : `-${Add<N, B>}` extends `${infer R extends number}`
+    ? R
+    : never
+  : `${B}` extends `-${infer M extends number}`
+  ? Length<[...NumToArr<A>, ...NumToArr<M>]>
+  : NumToArr<A> extends [...NumToArr<B>, ...infer R]
   ? Length<R>
   : NumToArr<B> extends [...NumToArr<A>, ...infer R]
   ? `-${Length<R>}` extends `${infer M extends number}`
@@ -33,14 +41,26 @@ export type Subtract<A extends number, B extends number> = NumToArr<A> extends [
   : never;
 
 // 掛け算
-export type Multiply<A extends number, B extends number> = MultiHelper<A, B, 0>;
+export type Multiply<A extends number, B extends number> = MultiHelper<A, B>;
 
 // 掛け算のヘルパー
 type MultiHelper<
   X extends number,
   Y extends number,
-  A extends number,
-> = Y extends 0 ? A : MultiHelper<X, Subtract<Y, 1>, Add<X, A>>;
+  A extends number = 0,
+> = `${X}` extends `-${infer N extends number}`
+  ? `${Y}` extends `-${infer M extends number}`
+    ? MultiHelper<N, M, A>
+    : `-${MultiHelper<N, Y, A>}` extends `${infer R extends number}`
+    ? R
+    : never
+  : `${Y}` extends `-${infer M extends number}`
+  ? `-${MultiHelper<X, M, A>}` extends `${infer R extends number}`
+    ? R
+    : never
+  : Y extends 0
+  ? A
+  : MultiHelper<X, Subtract<Y, 1>, Add<X, A>>;
 
 // 割り算
 export type Divide<A extends number, B extends number> = DivideHelper<A, B, 0>;
