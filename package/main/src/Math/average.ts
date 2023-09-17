9130afd2e549c13215609a079d05cd0510f6a6f4
Riya Amemiya <riyaamemiya@gmail.com>
2023-09-18 00:55:04 +0900

    fix(Math): 平均関数の空配列への対応とテストケースの追加

--- package/main/src/Math/average.ts
+++ package/main/src/Math/average.ts
@@ -6,6 +6,10 @@ import { division } from "./division";
  * @returns number
  */
 export const average = (numbers: number[]): number => {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
   const sum = numbers.reduce((a, b) => addition(a, b), 0);
   const avg = division(sum, numbers.length);
   return avg;
