181af81c3632664972f9fb0e908a91178360af66
Riya Amemiya <riyaamemiya@gmail.com>
2023-09-18 00:23:44 +0900

    test(Math): 数学モジュールの足し算テストを詳細化

--- package/main/src/tests/Math/addition.test.ts
+++ package/main/src/tests/Math/addition.test.ts
@@ -1,5 +1,43 @@
 import { addition } from "@/Math/addition";
-test("{addition}", () => {
-  expect(addition(1, 2)).toBe(3);
-  expect(addition(1.1, 2.2)).toBe(3.3);
+
+describe("addition", () => {
+  it("should add two positive integers", () => {
+    expect(addition(2, 3)).toBe(5);
+  });
+
+  it("should add two negative integers", () => {
+    expect(addition(-2, -3)).toBe(-5);
+  });
+
+  it("should add a positive and a negative integer", () => {
+    expect(addition(2, -3)).toBe(-1);
+  });
+
+  it("should add two positive decimal numbers", () => {
+    expect(addition(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+
+  it("should add two negative decimal numbers", () => {
+    expect(addition(-0.1, -0.2)).toBeCloseTo(-0.3);
+  });
+
+  it("should add a positive and a negative decimal number", () => {
+    expect(addition(0.1, -0.2)).toBeCloseTo(-0.1);
+  });
+
+  it("should add a positive integer and a positive decimal number", () => {
+    expect(addition(2, 0.3)).toBeCloseTo(2.3);
+  });
+
+  it("should add a negative integer and a negative decimal number", () => {
+    expect(addition(-2, -0.3)).toBeCloseTo(-2.3);
+  });
+
+  it("should add a positive integer and a negative decimal number", () => {
+    expect(addition(2, -0.3)).toBeCloseTo(1.7);
+  });
+
+  it("should add a negative integer and a positive decimal number", () => {
+    expect(addition(-2, 0.3)).toBeCloseTo(-1.7);
+  });
 });
