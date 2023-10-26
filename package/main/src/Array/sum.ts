425bb2deb68b8c769c137844e27a2b48994394da
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-26 11:00:33 +0900

    docs(Array/sum.ts): 関数のドキュメンテーションコメントを追加

--- package/main/src/Array/sum.ts
+++ package/main/src/Array/sum.ts
@@ -1,4 +1,9 @@
 import { addition } from "@/Math/addition";
+/**
+ * 配列の合計を返す
+ * @param {number[]} x 数値型の配列
+ * @returns 配列の合計
+ */
 export const sum = (x: number[]) => {
   return x.reduce((a, b) => addition(a, b));
 };
