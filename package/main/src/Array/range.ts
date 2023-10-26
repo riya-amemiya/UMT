3e876afdcff5b16794515db8105a3a94f4b85302
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-26 12:08:44 +0900

    fix(range): startからendまでの連続した数字の配列を生成する際に、終了値が含まれるよう修正しました。

--- package/main/src/Array/range.ts
+++ package/main/src/Array/range.ts
@@ -6,14 +6,14 @@
  */
 function range(start: number, end?: number) {
   const arr = [];
-  if (!end) {
-    for (let i = 0; i <= start; i++) {
+  if (end) {
+    for (let i = start; i < end; i++) {
       arr.push(i);
     }
-    return arr;
-  }
-  for (let i = start; i <= end; i++) {
-    arr.push(i);
+  } else {
+    for (let i = 0; i < start; i++) {
+      arr.push(i);
+    }
   }
   return arr;
 }
--- package/main/src/tests/Array/range.test.ts
+++ package/main/src/tests/Array/range.test.ts
@@ -3,27 +3,22 @@ import { range } from "@/Array/range";
 describe("range", () => {
   test("-5から5までの連続した数字の配列を生成する。", () => {
     const result = range(-5, 5);
-    expect(result).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]);
+    expect(result).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]);
   });
 
-  test("0から0までの連続した数字の配列を生成する。", () => {
-    const result = range(0, 0);
-    expect(result).toEqual([0]);
-  });
-
-  test("0から5までの連続した数字の配列を生成する。", () => {
+  test("1から10までの連続した数字の配列を生成する。", () => {
     const result = range(1, 10);
 
-    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
-  test("5から5までの連続した数字の配列を生成する。", () => {
-    const result = range(5, 5);
-    expect(result).toEqual([5]);
+  test("空の配列が出力される。", () => {
+    expect(range(0, 0)).toEqual([]);
+    expect(range(5, 5)).toEqual([]);
   });
 
   test("0から5までの連続した数字の配列を生成する。", () => {
     const result = range(5);
-    expect(result).toEqual([0, 1, 2, 3, 4, 5]);
+    expect(result).toEqual([0, 1, 2, 3, 4]);
   });
 });
