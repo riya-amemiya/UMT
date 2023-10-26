7a77e00c5fa6d4eb1d7cd8ded0b74c2d37573f53
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-26 10:57:52 +0900

    feat: rangeAdvance関数にドキュメンテーションを追加

--- package/main/src/Advance/rangeAdvance.ts
+++ package/main/src/Advance/rangeAdvance.ts
@@ -1,3 +1,10 @@
+/**
+ * 条件式を満たす数値の配列を返す
+ * @param start 開始数値
+ * @param end 終了数値
+ * @param conditionalExpression 条件式
+ * @returns
+ */
 export const rangeAdvance = (
   start: number,
   end?: number,
@@ -6,20 +13,16 @@ export const rangeAdvance = (
   const arr = [];
   if (!end) {
     for (let i = 0; i <= start; i++) {
-      if (conditionalExpression) {
-        if (conditionalExpression(i)) {
-          arr.push(i);
-        }
-      }
+      conditionalExpression
+        ? conditionalExpression(i) && arr.push(i)
+        : arr.push(i);
     }
     return arr;
   }
   for (let i = start; i <= end; i++) {
-    if (conditionalExpression) {
-      if (conditionalExpression(i)) {
-        arr.push(i);
-      }
-    }
+    conditionalExpression
+      ? conditionalExpression(i) && arr.push(i)
+      : arr.push(i);
   }
   return arr;
 };
