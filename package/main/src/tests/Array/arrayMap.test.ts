867c1e890b0d8c1b803b6d66809df85ac85b99a8
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-26 11:28:48 +0900

    test削除

--- package/main/src/tests/Array/arrayMap.test.ts
+++
@@ -1,8 +0,0 @@
-import { arrayMap } from "@/Array/arrayMap";
-
-test("{arrayMap}", () => {
-  expect(arrayMap([1, 2, 3], (value) => value * 2)).toEqual([2, 4, 6]);
-  expect(arrayMap([1, 2, 3], (value) => value * 2)).toHaveLength(3);
-  expect(arrayMap([1, 2, 3], (value) => value * 2)).not.toContain(1);
-  expect(arrayMap([1, 2, 3], (value) => value * 2)).not.toContain(3);
-});
