63531147e1edd6c69c7c9b1e18a723cc80c8d1a8
Riya Amemiya <riyaamemiya@gmail.com>
2023-10-29 03:28:48 +0900

    アプデ

---
+++ package/main/src/tests/Tool/deleteSpace.test.ts
@@ -0,0 +1,38 @@
+import { deleteSpace } from "@/Tool/deleteSpace";
+
+describe("deleteSpace", () => {
+  it("should remove all spaces from a string", () => {
+    const input = "hello world";
+    const expectedOutput = "helloworld";
+    const actualOutput = deleteSpace(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it("should remove all spaces from an empty string", () => {
+    const input = "";
+    const expectedOutput = "";
+    const actualOutput = deleteSpace(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it("should remove all spaces from a string with leading and trailing spaces", () => {
+    const input = "  hello world  ";
+    const expectedOutput = "helloworld";
+    const actualOutput = deleteSpace(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it("should remove all spaces from a string with multiple spaces between words", () => {
+    const input = "hello   world";
+    const expectedOutput = "helloworld";
+    const actualOutput = deleteSpace(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it("should remove all spaces from a string with tabs and newlines", () => {
+    const input = "hello\t\nworld";
+    const expectedOutput = "helloworld";
+    const actualOutput = deleteSpace(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+});
