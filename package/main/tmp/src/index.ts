84cf2b39e8c11a3c881636d52ff5c43da64cd464
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-29 23:02:23 +0900

    テストフォルダ追加

---
+++ package/main/tmp/src/index.ts
@@ -0,0 +1,5 @@
+import { calculator } from "../../module";
+
+console.info("====================================");
+console.info(calculator("1-1-1-1"));
+console.info("====================================");
