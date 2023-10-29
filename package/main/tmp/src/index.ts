9ca30491301925ab1b0121b98aaee42255019a59
Riya Amemiya <riyaamemiya@gmail.com>
2023-10-29 23:08:58 +0900

    ちょっと更新

--- package/main/tmp/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { calculator } from "../../module";
-
-console.info("====================================");
-console.info(calculator("1-1-1-1"));
-console.info("====================================");
