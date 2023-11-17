dd7c0fdeacfd191b2cc181a670d02d82cf44b519
Yuta Nishi <riyaamemiya@gmail.com>
2023-11-17 14:58:16 +0900

    refactor(Array): クイックソートのロジックを改善し、テストケースを追加

--- package/main/src/Array/quickSort.ts
+++ package/main/src/Array/quickSort.ts
@@ -1,4 +1,3 @@
-import { random } from "../Math/random";
 /**
  * 配列を高速にソート
  * @param  {unknown[]} array 配列
@@ -7,37 +6,43 @@ import { random } from "../Math/random";
  * @returns unknown[]
  * @example quickSort([1, 3, 2, 4, 5]); // [1, 2, 3, 4, 5]
  */
-
 export const quickSort = <A extends unknown[]>(
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   array: any[],
   startID = 0,
   endID: number = array.length - 1,
 ): A => {
-  const pivot = array[random(endID, startID)];
+  if (startID >= endID) {
+    return array as A;
+  }
+
+  // 中央値をピボットとして選択
+  const pivotIndex = Math.floor((startID + endID) / 2);
+  const pivot = array[pivotIndex];
   let left = startID;
   let right = endID;
-  while (true) {
+
+  while (left <= right) {
     while (array[left] < pivot) {
       left++;
     }
-    while (pivot < array[right]) {
+    while (array[right] > pivot) {
       right--;
     }
-    if (right <= left) {
-      break;
+    if (left <= right) {
+      [array[left], array[right]] = [array[right], array[left]];
+      left++;
+      right--;
     }
-    const temporary = array[left];
-    array[left] = array[right];
-    array[right] = temporary;
-    left++;
-    right--;
   }
-  if (startID < left - 1) {
-    quickSort(array, startID, left - 1);
+
+  // より小さいサブ配列を先にソート
+  if (startID < right) {
+    quickSort(array, startID, right);
   }
-  if (right + 1 < endID) {
-    quickSort(array, right + 1, endID);
+  if (left < endID) {
+    quickSort(array, left, endID);
   }
+
   return array as A;
 };
--- package/main/src/tests/Array/quickSort.test.ts
+++ package/main/src/tests/Array/quickSort.test.ts
@@ -1,18 +1,33 @@
 import { quickSort } from "@/Array/quickSort";
-test("{quickSort}", () => {
-  expect(quickSort([1, 2, 3])).toEqual([1, 2, 3]);
-  expect(quickSort([3, 2, 1])).toEqual([1, 2, 3]);
-  expect(quickSort([1, 3, 2])).toEqual([1, 2, 3]);
-  expect(quickSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  ]);
-  expect(quickSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  ]);
-  expect(quickSort([10, 1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  ]);
-  expect(quickSort([1, 10, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  ]);
+describe("quickSort", () => {
+  it("空の配列をソートすると空の配列が返される", () => {
+    expect(quickSort([])).toEqual([]);
+  });
+
+  it("既にソートされた配列はそのまま返される", () => {
+    expect(quickSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("逆順にソートされた配列を正しくソートする", () => {
+    expect(quickSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("ランダムな配列を正しくソートする", () => {
+    expect(quickSort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])).toEqual([
+      1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9,
+    ]);
+  });
+
+  it("重複した要素を含む配列を正しくソートする", () => {
+    expect(quickSort([3, 3, 3, 2, 1, 1, 4, 4, 5])).toEqual([
+      1, 1, 2, 3, 3, 3, 4, 4, 5,
+    ]);
+  });
+  it("大量の要素を含む配列を正しくソートする", () => {
+    const largeArray = Array.from({ length: 10000 }, () =>
+      Math.floor(Math.random() * 10000),
+    );
+    const sortedArray = [...largeArray].sort((a, b) => a - b);
+    expect(quickSort(largeArray)).toEqual(sortedArray);
+  });
 });
