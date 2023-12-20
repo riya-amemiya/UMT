baefe495d3a9ec4dbca9a32ff12c8f4254125f4b
Yuta Nishi <riyaamemiya@gmail.com>
2023-12-20 12:13:09 +0900

    test(Array/quickSort): テストケースの可読性向上のため空行を追加

--- package/main/src/tests/Array/quickSort.test.ts
+++ package/main/src/tests/Array/quickSort.test.ts
@@ -17,11 +17,13 @@ describe("quickSort", () => {
       1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9,
     ]);
   });
+
   it("重複した要素を含む配列を正しくソートする", () => {
     expect(quickSort([3, 3, 3, 2, 1, 1, 4, 4, 5])).toEqual([
       1, 1, 2, 3, 3, 3, 4, 4, 5,
     ]);
   });
+
   it("大量の要素を含む配列を正しくソートする", () => {
     const largeArray = Array.from({ length: 10000 }, () =>
       Math.floor(Math.random() * 10000),
@@ -29,16 +31,19 @@ describe("quickSort", () => {
     const sortedArray = [...largeArray].sort((a, b) => a - b);
     expect(quickSort(largeArray)).toEqual(sortedArray);
   });
+
   it("負の数を含む配列を正しくソートする", () => {
     expect(quickSort([5, -1, 3, 2, 4, -5, 1, -2, 0])).toEqual([
       -5, -2, -1, 0, 1, 2, 3, 4, 5,
     ]);
   });
+
   it("配列を降順にソートする", () => {
     expect(quickSort([1, 2, 3, 4, 5], (a, b) => b - a)).toEqual([
       5, 4, 3, 2, 1,
     ]);
   });
+
   it("部分配列をソートする", () => {
     expect(quickSort([3, 1, 4, 1, 5], undefined, 1, 3)).toEqual([
       3, 1, 1, 4, 5,
@@ -48,6 +53,7 @@ describe("quickSort", () => {
   it("単一要素の配列が変更されずに返される", () => {
     expect(quickSort([1])).toEqual([1]);
   });
+
   it("compareFunctionが例外を投げる場合", () => {
     const throwCompare = () => {
       throw new Error("Error during comparison");
