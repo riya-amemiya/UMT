55cf1147903a93c4bb3a874ed983344879ac2e65
Yuta Nishi <riyaamemiya@gmail.com>
2023-12-25 23:26:44 +0900

    test(math): 数学関数のテストを追加および修正

--- package/umt_wasm/tests/math/test_math_separator.rs
+++ package/umt_wasm/tests/math/test_math_separator.rs
@@ -6,3 +6,15 @@ fn test_math_separator() {
     assert_eq!(math_separator(1234), vec![1000, 234]);
     assert_eq!(math_separator(12345), vec![10000, 2345]);
 }
+
+#[test]
+fn test_math_separator_zero() {
+    assert_eq!(math_separator(0), vec![0, 0]);
+}
+
+#[test]
+fn test_math_separator_single_digit() {
+    for i in 1..10 {
+        assert_eq!(math_separator(i), vec![0, i]);
+    }
+}
