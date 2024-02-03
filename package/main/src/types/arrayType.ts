140bee62639854afbe667f259cd932235b9dee5c
Riya Amemiya <riyaamemiya@gmail.com>
2024-02-03 10:43:45 +0900

    feat(Array): 配列の要素を組み合わせるzip関数を追加

--- package/main/src/types/arrayType.ts
+++ package/main/src/types/arrayType.ts
@@ -16,3 +16,13 @@ type Chunk<
     : [...R, C];
 
 export type ChunkArrayType<T, N extends number> = Chunk<T, N>;
+
+type ZIP<T, O extends unknown[] = []> = T extends [infer Head, ...infer Tail]
+  ? Head extends unknown[]
+    ? ZIP<Tail, [...O, Head[number]]>
+    : never
+  : T extends []
+    ? O[]
+    : T;
+
+export type ZipArrayType<T extends unknown[][]> = ZIP<T>;
