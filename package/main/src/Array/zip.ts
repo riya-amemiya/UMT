8e2f1746159f0804714470d873b9fd5bd393fdff
Riya Amemiya <riyaamemiya@gmail.com>
2024-02-03 10:51:40 +0900

    refactor(zip関数): 型定義を改善し、不要なany型とコメントを削除

--- package/main/src/Array/zip.ts
+++ package/main/src/Array/zip.ts
@@ -2,17 +2,15 @@ import type { ZipArrayType } from "$/arrayType";
 
 /**
  * 複数の配列を受け取り、それらの配列の要素を組み合わせて新しい配列を作成します。
- * @param {...Array<any>[]} arrays
- * @returns {Array<any[]>} 組み合わせた要素の新しい配列
+ * @param {T} arrays - 組み合わせる配列のリスト
+ * @returns {ZipArrayType<T>} - 各配列の要素が組み合わされた新しい配列
  */
 export function zip<T extends unknown[][]>(...arrays: T): ZipArrayType<T> {
   if (arrays.length === 0) {
     return [] as unknown as ZipArrayType<T>;
   }
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  const length = Math.min(...arrays.map((array: any) => array.length));
+  const length = Math.min(...arrays.map((array) => array.length));
   return Array.from({ length }, (_, index) => {
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    return arrays.map((array: any) => array[index]);
+    return arrays.map((array) => array[index]);
   }) as unknown as ZipArrayType<T>;
 }
