639b72e4cad57c98b363dc4db70e6298dc62f2f1
Riya Amemiya <riyaamemiya@gmail.com>
2024-03-24 17:26:29 +0900

    refactor(Tool): rgbaToHsla関数の引数のデータ構造を改善し、変数名を明確化

--- package/main/src/Tool/rgbaToHsla.ts
+++ package/main/src/Tool/rgbaToHsla.ts
@@ -10,18 +10,23 @@ import { subtract } from "@/Math/subtract";
  * @returns { h: number; s: number; l: number; a: number; }
  * @example rgbaToHsla({ r: 100, g: 100, b: 100, a: 1 }); // { h: 0, s: 0, l: 39.22, a: 1 }
  */
-export const rgbaToHsla = (rgba: {
+export const rgbaToHsla = ({
+  r,
+  g,
+  b,
+  a = 1,
+}: {
   r: number;
   g: number;
   b: number;
   a?: number;
-}) => {
-  const r = division(rgba.r, 255);
-  const g = division(rgba.g, 255);
-  const b = division(rgba.b, 255);
+}): { h: number; s: number; l: number; a: number } => {
+  const rPrime = division(r, 255);
+  const gPrime = division(g, 255);
+  const bPrime = division(b, 255);
 
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
+  const max = Math.max(rPrime, gPrime, bPrime);
+  const min = Math.min(rPrime, gPrime, bPrime);
   const diff = subtract(max, min);
 
   let h = 0;
@@ -33,16 +38,19 @@ export const rgbaToHsla = (rgba: {
 
   if (diff !== 0) {
     switch (max) {
-      case r: {
-        h = addition(division(subtract(g, b), diff), g < b ? 6 : 0);
+      case rPrime: {
+        h = addition(
+          division(subtract(gPrime, bPrime), diff),
+          gPrime < bPrime ? 6 : 0,
+        );
         break;
       }
-      case g: {
-        h = addition(division(subtract(b, r), diff), 2);
+      case gPrime: {
+        h = addition(division(subtract(bPrime, rPrime), diff), 2);
         break;
       }
-      case b: {
-        h = addition(division(subtract(r, g), diff), 4);
+      case bPrime: {
+        h = addition(division(subtract(rPrime, gPrime), diff), 4);
         break;
       }
     }
@@ -53,6 +61,6 @@ export const rgbaToHsla = (rgba: {
     h: roundOf(h, 2),
     s: roundOf(s * 100, 2),
     l: roundOf(l * 100, 2),
-    a: rgba.a || 1,
+    a,
   };
 };
