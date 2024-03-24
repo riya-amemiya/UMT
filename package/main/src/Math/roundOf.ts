ede4392b855b19943d6d50281a3874030ccaa8ff
Riya Amemiya <riyaamemiya@gmail.com>
2024-03-24 18:28:30 +0900

    feat(Math): デフォルトの精度を追加し、関連するテストを更新する

--- package/main/src/Math/roundOf.ts
+++ package/main/src/Math/roundOf.ts
@@ -5,6 +5,6 @@
  * @returns number
  * @example roundOf(1.234, 2); // 1.23
  */
-export const roundOf = (number_: number, precision: number) => {
+export const roundOf = (number_: number, precision = 0) => {
   return Math.round(number_ * 10 ** precision) / 10 ** precision;
 };
