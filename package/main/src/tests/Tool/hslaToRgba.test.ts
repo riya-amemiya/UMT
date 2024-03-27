987388277421ac2e106907450f3dd5c7895b8680
Yuta Nishi <riyaamemiya@gmail.com>
2024-03-27 16:12:51 +0900

    refactor(hslaToRgba): 小数点以下2桁までの丸め処理を追加し、新しいテストケースを実装

--- package/main/src/tests/Tool/hslaToRgba.test.ts
+++ package/main/src/tests/Tool/hslaToRgba.test.ts
@@ -15,6 +15,26 @@ describe("hslaToRgba関数のテスト", () => {
     expect(hslaToRgba(h, s, l, a)).toEqual(expected);
   });
 
+  it.each([
+    [0, 0, 0, { r: 0, g: 0, b: 0, a: 1 }],
+    [360, 100, 100, { r: 255, g: 255, b: 255, a: 1 }],
+    [0, 100, 50, { r: 255, g: 0, b: 0, a: 1 }],
+  ])("HSLA(%i, %i%, %i%)をRGBAに変換する", (h, s, l, expected) => {
+    expect(hslaToRgba(h, s, l)).toEqual(expected);
+  });
+
+  it.each([
+    [240, 100, 25, 1, { r: 0, g: 0, b: 127.5, a: 1 }],
+    [120, 100, 25, 1, { r: 0, g: 127.5, b: 0, a: 1 }],
+    [0, 100, 25, 1, { r: 127.5, g: 0, b: 0, a: 1 }],
+    [60, 100, 25, 1, { r: 127.5, g: 127.5, b: 0, a: 1 }],
+    [180, 100, 25, 1, { r: 0, g: 127.5, b: 127.5, a: 1 }],
+    [300, 100, 25, 1, { r: 127.5, g: 0, b: 127.5, a: 1 }],
+    [0, 0, 25, 1, { r: 63.75, g: 63.75, b: 63.75, a: 1 }],
+  ])("HSLA(%i, %i%, %i%, %i)をRGBAに変換する", (h, s, l, a, expected) => {
+    expect(hslaToRgba(h, s, l, a)).toEqual(expected);
+  });
+
   it("不正な値を渡した場合の挙動", () => {
     expect(hslaToRgba(-60, 100, 50, 1)).toEqual({ r: 0, g: 0, b: 0, a: 1 });
     expect(hslaToRgba(420, 100, 50, 1)).toEqual({ r: 0, g: 0, b: 0, a: 1 });
