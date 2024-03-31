d0f4f432cc6d3fa5d33301a684408c1568dc35b5
Yuta Nishi <riyaamemiya@gmail.com>
2024-03-31 22:36:56 +0900

    refactor(Math): ラジアンから角度への変換関数とそのテストを改善

--- package/main/src/Math/radToDeg.ts
+++ package/main/src/Math/radToDeg.ts
@@ -1,7 +1,9 @@
+import { division } from "./division";
+
 /**
  * ラジアンを角度に変換
  * @param  {number} x
  * @returns number
  * @example radToDeg(Math.PI); // 180
  */
-export const radToDeg = (x: number) => x / (Math.PI / 180);
+export const radToDeg = (x: number) => division(x, division(Math.PI, 180));
