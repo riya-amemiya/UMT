654751e48399110290a7b4a9d8a56e9879cca55b
Riya Amemiya <riyaamemiya@gmail.com>
2024-05-19 23:36:34 +0900

    feat: オブジェクトの指定パスを判定する`has`関数とテスト追加

---
+++ package/main/src/Object/has.ts
@@ -0,0 +1,29 @@
+/**
+ * オブジェクトが指定されたパスを持っているかどうかを判定する
+ * @param {T} object - 判定するオブジェクト
+ * @param {string | string[]} path - 判定するパス
+ * @returns {boolean} パスが存在すればtrue、そうでなければfalse
+ * @example has({ a: { b: 1 } }, "a.b"); // true
+ * has({ a: { b: 1 } }, ["a", "b"]); // true
+ * has({ a: { b: 1 } }, "a.c"); // false
+ */
+export const has = <T extends { [key: string]: unknown }>(
+  object: T,
+  path: string | string[],
+): boolean => {
+  if (typeof path === "string") {
+    path = path.split(".");
+  }
+
+  let current = { ...object };
+  for (const key of path) {
+    if (
+      current == null ||
+      !Object.prototype.hasOwnProperty.call(current, key)
+    ) {
+      return false;
+    }
+    current = current[key] as T;
+  }
+  return true;
+};
