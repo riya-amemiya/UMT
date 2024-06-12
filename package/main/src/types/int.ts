eedb8599973fc8eb6c55b5a82fcf34e0513c4588
Riya Amemiya <riyaamemiya@gmail.com>
2024-06-12 23:07:18 +0900

    refactor(types): int型をevenとoddに分割し、instWithOutZero型を追加

--- package/main/src/types/int.ts
+++ package/main/src/types/int.ts
@@ -1,4 +1,11 @@
-export type int = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type even = 0 | 2 | 4 | 6 | 8;
+export type odd = 1 | 3 | 5 | 7 | 9;
+export type instWithOutZero = even | odd extends infer U
+  ? U extends 0
+    ? never
+    : U
+  : never;
+export type int = even | odd;
 
 export type upToTwenty = 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
 
