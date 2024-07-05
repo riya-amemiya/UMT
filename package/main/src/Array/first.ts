a041069b4b349fc8ca2a6cca4c9823a405748488
Yuta Nishi <riyaamemiya@gmail.com>
2024-07-05 11:43:55 +0900

    refactor(Array): 型定義を更新し、関数の戻り値の型を改善

--- package/main/src/Array/first.ts
+++ package/main/src/Array/first.ts
@@ -1,9 +1,15 @@
+import type { First } from "$/logicType";
+
 /**
  * 配列の先頭の要素を返す
  * @param array 配列
  * @returns 配列の先頭の要素
  * @example first([1, 2, 3]); // 1
  */
-export const first = <T>(array: T[]): T | undefined => {
-  return array.length > 0 ? array[0] : undefined;
+export const first = <T extends unknown[]>(
+  array: T,
+): First<T> extends never ? T[number] | undefined : First<T> => {
+  return (array.length > 0 ? array[0] : undefined) as First<T> extends never
+    ? T[number] | undefined
+    : First<T>;
 };
