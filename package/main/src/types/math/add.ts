14e9d12dd49d027beb407ea5bc3b96a45d854d98
Yuta Nishi <riyaamemiya@gmail.com>
2024-07-08 17:43:42 +0900

    refactor(types): ディレクトリ構造を整理し型定義を分割

---
+++ package/main/src/types/math/add.ts
@@ -0,0 +1,18 @@
+import type { NumberToArray } from "./numberToArray";
+import type { Subtract } from "./subtract";
+
+import type { Length } from "$/logicType";
+
+// たし算
+export type Add<
+  X extends number,
+  Y extends number,
+> = `${X}` extends `-${infer N extends number}`
+  ? `${Y}` extends `-${infer M extends number}`
+    ? `-${Add<N, M>}` extends `${infer R extends number}`
+      ? R
+      : never
+    : Subtract<Y, N>
+  : `${Y}` extends `-${infer M extends number}`
+    ? Subtract<X, M>
+    : Length<[...NumberToArray<X>, ...NumberToArray<Y>]>;
