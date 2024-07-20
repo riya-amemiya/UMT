aeff0b601821c1363f3d57a3b15d754b5a094ef1
Riya Amemiya <riyaamemiya@gmail.com>
2024-07-20 14:48:26 +0900

    refactor(types): jokeType.tsを新しいディレクトリ構造に移動

--- package/main/src/types/index.ts
+++ package/main/src/types/index.ts
@@ -2,6 +2,6 @@ export * from "./array";
 export * from "./clockType";
 export * from "./dateType";
 export * from "./int";
-export * from "./jokeType";
+export * from "./joke";
 export * from "./logic";
 export * from "./math";
