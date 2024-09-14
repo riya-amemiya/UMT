02171b7cc4dfbeb549f9adde1b134df7ee976d17
Yuta Nishi <riyaamemiya@gmail.com>
2024-09-14 21:02:36 +0900

    fix(UA): Androidデバイスの判別ロジックを修正し、タブレットとモバイルを適切に区別

--- package/main/src/UA/extractDeviceFromUserAgent.ts
+++ package/main/src/UA/extractDeviceFromUserAgent.ts
@@ -6,18 +6,25 @@ export const extractDeviceFromUserAgent = (
   if (/bot|googlebot|crawler|spider|robot|crawling/i.test(ua)) {
     return "bot";
   }
-  if (
-    /iphone|ipod|android.*mobile|mobile.*android|webos|blackberry|iemobile|opera mini/i.test(
-      ua,
-    )
-  ) {
+
+  if (/iphone|ipod|webos|blackberry|iemobile|opera mini/i.test(ua)) {
     return "mobile";
   }
+
+  if (/android/i.test(ua)) {
+    if (/mobile/i.test(ua)) {
+      return "mobile";
+    }
+    return "tablet";
+  }
+
   if (/ipad|android(?!.*mobile)/i.test(ua)) {
     return "tablet";
   }
+
   if (/windows|macintosh|linux/i.test(ua)) {
     return "desktop";
   }
+
   return "other";
 };
