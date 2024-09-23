29b13700d4ef38b8ec2fc6ca4ce6ef1b95c0cfb1
Riya Amemiya <riyaamemiya@gmail.com>
2024-09-23 17:18:37 +0900

    refactor(Validate): 判定関数を即時評価から関数に変更

--- package/main/src/Validate/isBun.ts
+++ package/main/src/Validate/isBun.ts
@@ -1,11 +1,11 @@
-let _isBun: boolean;
-try {
-  // @ts-expect-error: Bun is only defined in Bun runtime
-  _isBun = typeof Bun !== "undefined";
-} catch {
-  _isBun = false;
-}
 /**
  * bun環境かどうかを判定します。
  */
-export const isBun = _isBun;
+export const isBun = () => {
+  try {
+    // @ts-expect-error: Bun is only defined in Bun runtime
+    return typeof Bun !== "undefined";
+  } catch {
+    return false;
+  }
+};
