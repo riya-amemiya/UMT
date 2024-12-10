ec2c9ccf8726f40dbc3eff2a3327563007ab4775
Riya Amemiya <riyaamemiya@gmail.com>
2024-12-10 23:10:12 +0900

    feat: Rustエディションを2024に更新し、LCM関数と線形合同生成器を追加

--- package/umt_rust/tests/math/test_softmax.rs
+++ package/umt_rust/tests/math/test_softmax.rs
@@ -2,24 +2,19 @@ use umt_rust::math::umt_softmax;
 
 #[test]
 fn test_softmax() {
-    assert_eq!(
-        umt_softmax(vec![0.0, 0.0, 0.0], 3),
-        vec![0.333, 0.333, 0.333]
-    );
-    assert_eq!(
-        umt_softmax(vec![1.0, 1.0, 1.0], 3),
-        vec![0.333, 0.333, 0.333]
-    );
-    assert_eq!(
-        umt_softmax(vec![1.0, 2.0, 3.0], 3),
-        vec![0.090, 0.245, 0.665]
-    );
-    assert_eq!(
-        umt_softmax(vec![1.0, 2.0, 3.0, 4.0], 3),
-        vec![0.032, 0.087, 0.237, 0.644]
-    );
-    assert_eq!(
-        umt_softmax(vec![1.0, 2.0, 3.0, 4.0, 5.0], 3),
-        vec![0.012, 0.032, 0.086, 0.234, 0.636]
-    );
+    assert_eq!(umt_softmax(vec![0.0, 0.0, 0.0], 3), vec![
+        0.333, 0.333, 0.333
+    ]);
+    assert_eq!(umt_softmax(vec![1.0, 1.0, 1.0], 3), vec![
+        0.333, 0.333, 0.333
+    ]);
+    assert_eq!(umt_softmax(vec![1.0, 2.0, 3.0], 3), vec![
+        0.090, 0.245, 0.665
+    ]);
+    assert_eq!(umt_softmax(vec![1.0, 2.0, 3.0, 4.0], 3), vec![
+        0.032, 0.087, 0.237, 0.644
+    ]);
+    assert_eq!(umt_softmax(vec![1.0, 2.0, 3.0, 4.0, 5.0], 3), vec![
+        0.012, 0.032, 0.086, 0.234, 0.636
+    ]);
 }
