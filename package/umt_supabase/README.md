65095fdc3794c12d8bd231244bbd97e2403f4a54
Yuta Nishi <riyaamemiya@gmail.com>
2025-01-30 12:33:05 +0900

    feat(umt_supabase): 初期コミット - UMT Supabaseパッケージを追加し、必要なファイルと設定を含める

---
+++ package/umt_supabase/README.md
@@ -0,0 +1,34 @@
+# UMT Supabase Package
+
+Amemiya Riyaの便利関数集
+
+UMT = Useful My Tools
+
+アプデで破壊的変更がある可能性があります。
+
+## 実装方針
+
+- 依存関係を持たない
+- 拡張性を持たせるために、最小限の機能のみを実装する
+
+## Doc
+
+- [Doc](https://umt-main.oshaburikitchin.com/)
+
+## Install
+
+```bash
+npm install umt-supabase
+
+# or
+
+yarn add umt-supabase
+
+# or
+
+pnpm add umt-supabase
+
+# or
+
+bun add umt-supabase
+```
---
+++ package/umt_supabase/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/tests"],
+  cacheDirectory: "<rootDir>/node_modules/.cache/jest",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/Date/**/*.ts",
+    "!<rootDir>/**/{isBrowser,isNode,isNodeWebkit,isBun}.ts",
+  ],
+  coverageDirectory: "./coverage",
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
+export default config;
