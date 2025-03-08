85722e0c9344e156c57c547c4f25b887eb191cde
Yuta Nishi <riyaamemiya@gmail.com>
2025-03-08 20:29:56 +0900

    docs: 説明書に新しい例を追加して、`groupBy`関数の使い方を明確にする

--- package/main/src/Array/groupBy.ts
+++ package/main/src/Array/groupBy.ts
@@ -5,7 +5,8 @@
  * @returns Object with grouped elements
  * @example
  * groupBy([6.1, 4.2, 6.3], Math.floor); // { '4': [4.2], '6': [6.1, 6.3] }
- * groupBy(['one', 'two', 'three'], 'length'); // { '3': ['one', 'two'], '5': ['three'] }
+ * groupBy(["one", "two", "three"], (str) => str.length); // { '3': ['one', 'two'], '5': ['three'] }
+ * groupBy(["apple", "banana", "carrot"], (str) => str[0]); // { 'a': ['apple'], 'b': ['banana'], 'c': ['carrot'] }
  */
 export const groupBy = <T, K extends string | number>(
   array: T[],
